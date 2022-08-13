987974fec44d9b3768bc5f397d6ab7993b78e33a
unknown <tareqabuaqlain1@gmail.com>
2022-08-13 16:48:43 +0300

    edit port in server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const router = require('./router')
 const server = http.createServer(router)
-const port = 3000;
+const port = process.env.PORT || 4000;
 server.listen(port, () => {
     console.log(`Server is listening on port ${port}. Ready to accept requests!`);
 });
